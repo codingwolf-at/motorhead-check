5e0b4a7c4fe64f3747a6d61e74aa72ae5cfa1f91
codingwolf-at <atameshwari9@gmail.com>
2021-05-14 16:04:30 +0530

    added: state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import {useState} from "react";
 import './App.css';
 import { Header } from './components';
 
 function App() {
+  const [score, setScore] = useState(0);
   return (
     <div className="App">
-      <Header userName={"Atul Tameshwari"} userScore={0} />
+      <Header userName={"Atul Tameshwari"} userScore={score} />
+      <button 
+        onClick={() => setScore(score => score+1)}
+        style={{padding: "0.5rem", margin: "0.5rem"}}
+      >Incr</button>
+      <button 
+        onClick={() => setScore(score => score-1)}
+        style={{padding: "0.5rem", margin: "0.5rem"}}
+      >Decr</button>
     </div>
   );
 }
