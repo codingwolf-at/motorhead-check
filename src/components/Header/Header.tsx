0c16c27436e0fd8816c4225886292bc07f96ce6c
codingwolf-at <atameshwari9@gmail.com>
2021-05-14 16:05:02 +0530

    style: basic header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-
-type HeaderProps = {
-  userName: string;
-  userScore: number;
-}
+import "./Header.css"
+import {HeaderProps} from "./HeaderProps.types"
 
 export function Header({userName, userScore}: HeaderProps) {
   return (
-    <div>
-      <h1>This is header</h1>
-      <h2>Hello {userName}!</h2>
-      <h2>Your Score: {userScore}</h2>
+    <div className="header">
+      <h1 className="main-heading">Motor-Head Check</h1>
+      <h2>Hello, {userName}!</h2>
+      <h2>Your Score: {userScore} points</h2>
     </div>
   )
 }
