529310109465d4f964e1a86aa3036bd1978bb7cb
codingwolf-at <atameshwari9@gmail.com>
2021-05-17 00:30:00 +0530

    added: current question text

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import {useState} from "react";
+import {useReducer} from "react";
 import './App.css';
 import { Header } from './components';
 import {Button} from "@chakra-ui/react";
 
 function App() {
-  const [score, setScore] = useState(0);
+  const initialState = {
+    score: 0,
+    currentQueNo: 1
+  }
+
+  type ACTIONTYPE = 
+    | {type: "INCREMENT"; payload: number}
+    | {type: "DECREMENT"; payload: number};
+
+  const quizReducer = (state: typeof initialState, action: ACTIONTYPE): typeof initialState => {
+    switch (action.type) {
+      case "INCREMENT":
+        return {
+          ...state,
+          score: state.score + action.payload,
+          currentQueNo: state.currentQueNo + 1
+        };
+      case "DECREMENT":
+        return {
+          ...state,
+          score: state.score - action.payload,
+          currentQueNo: state.currentQueNo + 1
+        };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(quizReducer, initialState);
+
   return (
     <div className="App">
-      <Header userName={"Atul Tameshwari"} userScore={score} />
+      <Header userName={"Atul Tameshwari"} userScore={state.score} currentQueNo={state.currentQueNo} />
       <Button 
-        onClick={() => setScore(score => score+1)}
+        onClick={() => dispatch({type: "INCREMENT", payload: 1})}
       >Incr</Button>
       <Button 
-        onClick={() => setScore(score => score-1)}
+        onClick={() => dispatch({type: "DECREMENT", payload: 1})}
       >Decr</Button>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,18 @@ import {HeaderProps} from "./HeaderProps.types"
 import {Container, Heading, Text} from "@chakra-ui/react";
 
 
-export function Header({userName, userScore}: HeaderProps) {
+export function Header({userName, userScore, currentQueNo}: HeaderProps) {
   return (
     <Container p={3} maxW="container.lg" bg="gray.100">
-      <Heading align="center">Quiz</Heading>
+      <Heading align="center" mb={2}>Motor-Head Check ✔</Heading>
       <Text fontSize="xl" align="right">
         Hello {userName}!
       </Text>
       <Text fontSize="xl" align="right">
         Your Score: {userScore}
+      </Text>
+      <Text fontSize="xl" align="right">
+        Current Question: {currentQueNo}
       </Text>
     </Container>
   )
